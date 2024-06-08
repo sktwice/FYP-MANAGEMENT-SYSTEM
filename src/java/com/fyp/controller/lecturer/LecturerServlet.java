0974ec561f484308de0497e917fe3fb604f43597
zahidi <167437083+sktwice@users.noreply.github.com>
2024-06-09 00:48:14 +0800

    Update LecturerServlet.java

--- src/java/com/fyp/controller/lecturer/LecturerServlet.java
+++ src/java/com/fyp/controller/lecturer/LecturerServlet.java
@@ -7,23 +7,19 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
 import com.fyp.lecturer.dao.LecturerDAO;
 import com.fyp.dao.student.ProposalSvDAO;
 import com.fyp.model.bean.Lecturer;
-import com.fyp.model.bean.Scope;
 
 
 public class LecturerServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private LecturerDAO lecturerDAO;
-    private ProposalSvDAO proposalSvDAO;
 
     @Override
     public void init() {
         lecturerDAO = new LecturerDAO();
-        proposalSvDAO = new ProposalSvDAO();
     }
 
     @Override
@@ -35,16 +31,10 @@ public class LecturerServlet extends HttpServlet {
             System.out.println("Servlet: Number of lecturers retrieved: " + listLecturer.size());
             request.setAttribute("listLecturer", listLecturer);
             
-            // Retrieve list of scopes
-            List<Scope> scopeList = proposalSvDAO.getAllScopes();
-            request.setAttribute("scopeList", scopeList);
             
             // Forward request to the JSP
             RequestDispatcher dispatcher = request.getRequestDispatcher("Lecturer.jsp");
             dispatcher.forward(request, response);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database error");
         } catch (Exception e) {
             e.printStackTrace();
             throw new ServletException("Error retrieving data", e);
